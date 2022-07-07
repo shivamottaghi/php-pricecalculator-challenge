4cf4d5d6661109a251833296433686aaf24935c1
shivamtg <shivamtg1376@gmail.com>
2022-07-07 11:24:55 +0200

    modify requirements in index

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
 <?php
+require 'vendor/autoload.php';
+require 'controller/homePageController.php';
+require 'model/Customer.php';
+require 'view/homePageView.php';
+require 'model/DBConnection.php';
+
+
+$controller = new homePageController();
+
