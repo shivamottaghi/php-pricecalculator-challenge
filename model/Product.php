d56601f979baac35314a4ec61af84c60f6f6214c
shivamtg <shivamtg1376@gmail.com>
2022-07-08 15:25:19 +0200

    add product class

--- model/Product.php
+++ model/Product.php
@@ -2,5 +2,29 @@
 
 class Product
 {
+    protected string $name;
+    protected float $price;
+
+    public function __construct(string $name , float $price)
+    {
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
 
 }
