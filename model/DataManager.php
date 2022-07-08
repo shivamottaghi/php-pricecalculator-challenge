d94d1fa8e9a777014f0ff31c01529c468f428d0b
shivamtg <shivamtg1376@gmail.com>
2022-07-08 10:24:13 +0200

    add class to manage data with a method to fetch products

--- model/DataManager.php
+++ model/DataManager.php
@@ -1,6 +1,20 @@
 <?php
-
 class DataManager
 {
+    protected DBConnection $dbc;
+    protected PDO $pdo;
+    protected array $products;
 
+    public function __construct()
+    {
+        $this->dbc = new DBConnection();
+        $this->pdo = $this->dbc->getDbc();
+    }
+
+    public function fetchProducts(){
+        $stmt = $this->pdo->query("select name , price from product");
+        $this->products = $stmt->fetchAll();
+
+        return $this->products;
+    }
 }
