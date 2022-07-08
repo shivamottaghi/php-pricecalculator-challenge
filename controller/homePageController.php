e25f33987658abdf5b54c4109d6fb985cea8e19d
shivamtg <shivamtg1376@gmail.com>
2022-07-08 15:23:43 +0200

    store products into cart using session

--- controller/homePageController.php
+++ controller/homePageController.php
@@ -7,7 +7,14 @@ class homePageController
     {
         $dbManage = new DataManager();
         $productList  = $dbManage->fetchProducts();
-
+        if (isset($_POST['buyProduct'])){
+            $name = $productList[$_POST['buyProduct']]['name'];
+            $price = $productList[$_POST['buyProduct']]['price']/100;
+            $addedToCart = new Product($name , $price);
+            array_push($_SESSION['cart'], $addedToCart);
+            var_dump($_SESSION['cart']);
+        }
+        //var_dump($_SESSION['cart']);
         include 'view/homePageView.php';
     }
 }
