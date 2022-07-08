33d085075b82cdbf8b3783b9ee931a6cd473f177
shivamtg <shivamtg1376@gmail.com>
2022-07-08 15:24:11 +0200

    add session

--- index.php
+++ index.php
@@ -4,8 +4,14 @@ require 'controller/homePageController.php';
 require 'model/Customer.php';
 require 'model/DBConnection.php';
 require 'model/DataManager.php';
+require 'model/Product.php';
+
+session_start();
 
 include 'view/header.php';
+if (!isset($_SESSION['cart'])){
+    $_SESSION['cart'] = [];
+}
 $controller = new homePageController();
 $controller->render();
 include 'view/footer.php';
