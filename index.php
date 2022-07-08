50afb0759bb8c25f40c4339c52a0e58f77cce465
shivamtg <shivamtg1376@gmail.com>
2022-07-08 10:20:57 +0200

    modify requirements

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 require 'vendor/autoload.php';
-require 'vendor/composer/autoload_real.php';
 require 'controller/homePageController.php';
 require 'model/Customer.php';
 require 'view/homePageView.php';
 require 'model/DBConnection.php';
+require 'model/DataManager.php';
 
 
 $controller = new homePageController();
+$controller->render();
 
