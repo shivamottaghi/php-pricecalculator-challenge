56670717009b3833d22803975852d4ae12383a9b
shivamtg <shivamtg1376@gmail.com>
2022-07-08 11:56:41 +0200

    modify requirements to avoid warnings

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 require 'vendor/autoload.php';
 require 'controller/homePageController.php';
 require 'model/Customer.php';
-require 'view/homePageView.php';
 require 'model/DBConnection.php';
 require 'model/DataManager.php';
 
-
+include 'view/header.php';
 $controller = new homePageController();
 $controller->render();
+include 'view/footer.php';
 
