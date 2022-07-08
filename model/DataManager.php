6d49c769137798b07c70d5b17f58d44cd1c4c4ae
shivamtg <shivamtg1376@gmail.com>
2022-07-08 17:40:47 +0200

    add method to fetch all the customers and a spicific customer

--- model/DataManager.php
+++ model/DataManager.php
@@ -4,6 +4,7 @@ class DataManager
     protected DBConnection $dbc;
     protected PDO $pdo;
     protected array $products;
+    protected array $customers;
 
     public function __construct()
     {
@@ -15,5 +16,16 @@ class DataManager
         $stmt = $this->pdo->query("select name , price from product");
         $this->products = $stmt->fetchAll();
         return $this->products;
+    }
+
+    public function fetchCustomers() : array{
+        $stmt = $this->pdo->query('select id , firstname , lastname from customer');
+        $this->customers = $stmt->fetchAll();
+        return $this->customers;
+    }
+
+    public function fetchOneCustomer($id) : array {
+        $stmt = $this->pdo->query('select * from customer where id =' .$id);
+        return $stmt->fetchAll();
     }
 }
