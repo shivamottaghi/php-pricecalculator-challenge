bf17f9433f5610c82b11998d73c6413844a80b24
shivamtg <shivamtg1376@gmail.com>
2022-07-11 10:20:36 +0200

    save customer object in session and load home page after login

--- controller/LogInController.php
+++ controller/LogInController.php
@@ -2,28 +2,26 @@
 
 class LogInController
 {
-    public function render(){
+    public function render()
+    {
         $dbManage = new DataManager();
         $customerList = $dbManage->fetchCustomers();
-        if(isset($_SESSION['login'])){
-            echo '<h1>you are already logged in! </h1>';
+        if (isset($_POST['submitLogIn'])) {
+            $id = $_POST['submitLogIn'];
+            $customer = $dbManage->fetchOneCustomer($id);
+            $_SESSION['login'] =
+            new Customer($customer[0]['firstname'], $customer[0]['lastname'], $customer[0]['group_id'] );
+            //$_SESSION['login'] = $customer;
+            var_dump($customer);
+            //$_SESSION['login'] = new Customer();
+            $controller = new homePageController();
+            $controller->render();
             require 'view/homePageView.php';
-        }else{
-            if (isset($_POST['submitLogIn'])){
-                $id = $_POST['submitLogIn'];
-                $customer = $dbManage->fetchOneCustomer($id);
-                //$_SESSION['login'] =
-                  //  new Customer($customer[0]['firstname'], $customer[0]['lastname'], $customer[0]['group_id'], $customer[0]['fixed_discount'], $customer[0]['variable_discount'] );
-                $_SESSION['login'] = $customer;
-                var_dump($customer);
-//                $_SESSION['login'] = new Customer();
-                //require 'view/homePageView.php';
-                return;
-            }else{
-                require 'view/logInView.php';
-            }
-            /// I will improve it later!
-            /// at this point I just want them to choose who they are so that I can calculate their discount
+        } else {
+            require 'view/logInView.php';
         }
+        /// I will improve it later!
+        /// at this point I just want them to choose who they are so that I can calculate their discount
+
     }
 }
