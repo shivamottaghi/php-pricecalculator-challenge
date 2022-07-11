c53c8406e87b27ba9c5012e2dc78463fdad7b69c
shivamtg <shivamtg1376@gmail.com>
2022-07-11 15:33:03 +0200

    remmove super globals from home page controller

--- controller/homePageController.php
+++ controller/homePageController.php
@@ -7,9 +7,9 @@ class homePageController
     {
         $dbManage = new DataManager();
         $productList  = $dbManage->fetchProducts();
-        if (isset($POST['buyProduct'])  && $POST['randcheck']==$_SESSION['rand']){
-            $name = $productList[$POST['buyProduct']]['name'];
-            $price = $productList[$POST['buyProduct']]['price']/100;
+        if (isset($POST['addToCart'])  && $POST['randcheck']==$_SESSION['rand']){
+            $name = $productList[$POST['addToCart']]['name'];
+            $price = $productList[$POST['addToCart']]['price']/100;
             $addedToCart = new Product($name , $price);
             array_push($_SESSION['cart'], $addedToCart);
             var_dump($_SESSION['cart']);
