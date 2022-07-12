8472bbe90d53f5df8916a748f6a6a4c0885b25f9
shivamtg <shivamtg1376@gmail.com>
2022-07-12 11:59:37 +0200

    fix merge conflict on submit order controller

--- controller/submitOrderController.php
+++ controller/submitOrderController.php
@@ -4,12 +4,6 @@ class submitOrderController
 {
     public function render(){
 
-<<<<<<< HEAD
         var_dump($_SESSION['login']);
         var_dump($_SESSION['cart']);
-=======
-//        var_dump($_SESSION['login']);
->>>>>>> a807a3bbb55d442479aa9ad29bcc686d2169f018
-//        $fixedSomething =  $_SESSION['login']->get;
-    }
 }
